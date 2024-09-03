0fa7fce24acb22a789995a4e4d9187a57c353d79
Max <interestinsic@mailbox.org>
2024-09-03 20:48:49 +0200

    minor changes

--- app/src/components/footer.js
+++ app/src/components/footer.js
@@ -7,10 +7,10 @@ function Footer() {
   <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
     <span class="ml-3 text-xl font-prata">Alice</span>
     <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2024 Personal Portfolio Site —
-      <a href="https://github.com/real-XAM" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">by @real-XAM</a>
+      <a href="https://github.com/real-XAM" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">by @interestinsic</a>
     </p>
     <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-      <a class="text-gray-500" href="https://github.com/real-XAM" target="_blank">
+      <a class="text-gray-500" href="https://github.com/interestinsic" target="_blank">
         <AiFillGithub size={30}/>
       </a>
     </span>
